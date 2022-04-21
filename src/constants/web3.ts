e4d0f7fa4b7bf1e77930ba3729c285f67635be7d
mingcrypto <mingli970909@gmail.com>
2022-04-21 11:21:43 +1000

    add deposit/withdraw limits

--- src/constants/web3.ts
+++ src/constants/web3.ts
@@ -1,2 +1,5 @@
 export const SIGN_MESSAGE =
   "Welcome to Hero Infinity! Please sign to continue.";
+export const MINIMUM_DEPOSIT = 20000;
+export const MINIMUM_WITHDRAW = 10000;
+export const WITHDRAW_FEE = 2000;
